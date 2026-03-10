5a1be3a9e9573a6d425630c90e4bb42672af6241
11 <shuhan1.25@intl.zju.edu.cn>
2026-03-10 03:46:42 +0100

    Add Practical4 variables.py(math + booleans)

---
+++ Practical 4/variables.py
@@ -0,0 +1,31 @@
+#Scotland population calculation
+#Populationin2004 (millions)
+a=5.08
+#Population in 2014 (millions)
+b=5.33
+#Population in 2024 (millions)
+c=5.55
+#Population change 2004-2014
+d=b-a
+#Population change 2014-2024
+e=c-b
+#Compare the size of d and e
+print("Population change 2004-2014:",d)
+print("Population change 2014-2024:",e) 
+print("Is d greater than e:",d>e)
+#d=0.25, e=0.22. e<d, growth slow down
+#Scotland's population growth is  slowing down: the population increase from 2014-2024 is less than that from 2004-2014.
+
+#4.2 Boolean operations
+#Define boolean variables X and Y
+X=True
+Y=False
+#Calculate X or Y and assign to W
+W=X or Y
+#print the result of W
+print("Result of W=X or Y:",W)
+#True table for W
+#X=True,Y=True -> W=True
+#X=True,Y=False -> W=True
+#X=False, Y=True -> W=True
+#X=False, Y=False -> W=False
