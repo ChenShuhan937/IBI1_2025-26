f591d2590f4dd1cb296c6d968641be1b9c92b71d
11 <shuhan1.25@intl.zju.edu.cn>
2026-04-15 08:36:45 +0100

    Correct and add some comments in spatial_SIR.py

--- Practical 9/spatial_SIR.py
+++ Practical 9/spatial_SIR.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 100x100 grid: 0=Susceptible, 1=Infected, 2=Recovered
+# Make array of all susceptible population. 100x100 grid: 0=Susceptible, 1=Infected, 2=Recovered
 population = np.zeros((100, 100))
 # Random initial infection site
 outbreak = np.random.choice(range(100), 2)
@@ -17,19 +17,19 @@ dirs = [(-1,-1),(-1,0),(-1,1),
         (1,-1),(1,0),(1,1)]
 
 # Spatial SIR simulation
-# 1. Locate all infected cells in the grid
-# 2. For each infected cell, attempt to infect 8 neighbors
-# 3. Infected cells recover stochastically
+# 1. Locate all infected people in the grid
+# 2. For each infected person, attempt to infect 8 neighbors
+# 3. Infected person recovers stochastically
 # 4. Update grid states
 # 5. Plot grid every 10 steps
 for step in range(100):
     new_infect = []
     new_recover = []
-    # Find coordinates of all infected cells
+    # Find coordinates of all infected people
     inf_x, inf_y = np.where(population == 1)
     
     for x, y in zip(inf_x, inf_y):
-        # Spread infection to neighboring cells
+        # Spread infection to neighbors
         for dx, dy in dirs:
             nx = x + dx
             ny = y + dy
@@ -37,11 +37,11 @@ for step in range(100):
                 if population[nx, ny] == 0:
                     if np.random.rand() < beta:
                         new_infect.append((nx, ny))
-        # Stochastic recovery for infected cells
+        # Stochastic recovery for infected people
         if np.random.rand() < gamma:
             new_recover.append((x, y))
     
-    # Update cell states
+    # Update infected and recovered states
     for x, y in new_infect:
         population[x, y] = 1
     for x, y in new_recover:
